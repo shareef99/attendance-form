d088c4a42eedb66933923b31d9cb2f0c3cd5e0bb
shareef <nadeemshareef934@gmail.com>
2021-05-11 23:18:52 +0530

    Config tailwind css

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,7 @@
 export default function Home() {
-    return <h2>Hello master shareef</h2>;
+    return (
+        <h2 className="text-3xl font-black text-red-100">
+            Hello master shareef
+        </h2>
+    );
 }
