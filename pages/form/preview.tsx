c216186acd18526d8c3e0cf2554ff9e514c795d8
shareef <nadeemshareef934@gmail.com>
2022-04-14 18:07:36 +0530

    ✨ add edit button on preview page

--- pages/form/preview.tsx
+++ pages/form/preview.tsx
@@ -6,81 +6,70 @@ import classes from "../../styles/preview/preview.module.scss";
 interface Props {}
 
 const Preview = (props: Props) => {
-    // Context
-    const { questions, handleSetQuestions } = useQuestions();
+  // Context
+  const { questions, handleSetQuestions } = useQuestions();
 
-    // Others
-    console.log(questions);
+  // Others
+  console.log(questions);
 
-    const handleFormSubmit = () => {
-        const questionsWithRequired = questions.filter(
-            (question) => question.isRequired
-        );
+  const handleFormSubmit = () => {
+    const questionsWithRequired = questions.filter(
+      (question) => question.isRequired
+    );
 
-        // const isAllAnswerExits = questionsWithRequired.every(
-        //     (x) => Boolean(x.answer) === true
-        // );
+    // const isAllAnswerExits = questionsWithRequired.every(
+    //     (x) => Boolean(x.answer) === true
+    // );
 
-        // const whichAnswersAreEmpty = questionsWithRequired.filter(
-        //     (x) => Boolean(x.answer) === false
-        // );
+    // const whichAnswersAreEmpty = questionsWithRequired.filter(
+    //     (x) => Boolean(x.answer) === false
+    // );
 
-        const idsOfIncorrectAnswers = questionsWithRequired
-            .filter((x) => Boolean(x.answer) === false)
-            .map((x) => x.id);
+    const idsOfIncorrectAnswers = questionsWithRequired
+      .filter((x) => Boolean(x.answer) === false)
+      .map((x) => x.id);
 
-        const addingErrorMessages = () => {
-            handleSetQuestions(
-                questions.map((question) =>
-                    idsOfIncorrectAnswers.includes(question.id)
-                        ? {
-                              ...question,
-                              errorMessage: "This is a required question",
-                          }
-                        : { ...question }
-                )
-            );
-        };
-
-        addingErrorMessages();
-
-        // console.log(
-        //     questionsWithRequired,
-        //     isAllAnswerExits,
-        //     whichAnswersAreEmpty,
-        //     idsOfIncorrectAnswers
-        // );
+    const addingErrorMessages = () => {
+      handleSetQuestions(
+        questions.map((question) =>
+          idsOfIncorrectAnswers.includes(question.id)
+            ? {
+                ...question,
+                errorMessage: "This is a required question",
+              }
+            : { ...question }
+        )
+      );
     };
 
-    return (
-        <section className="my-14">
-            {questions.length > 2 ? (
-                <>
-                    <QuestionsList questions={questions} preview />
-                    <div className="container flex md:max-w-[70%]">
-                        <button
-                            className={`${classes.submitButton} self-start -ml-8`}
-                            onClick={handleFormSubmit}
-                        >
-                            Submit
-                        </button>
-                    </div>
-                </>
-            ) : (
-                <div className="colCenter">
-                    <span className="text-red-400 font-medium text-lg">
-                        Failed to save your form
-                    </span>
-                    <div
-                        className="font-medium text-lg underline hover:no-underline focus:no-underline"
-                        style={{ marginTop: "100px" }}
-                    >
-                        <Link href="/form/new">Start a new form</Link>
-                    </div>
-                </div>
-            )}
-        </section>
-    );
+    addingErrorMessages();
+
+    // console.log(
+    //     questionsWithRequired,
+    //     isAllAnswerExits,
+    //     whichAnswersAreEmpty,
+    //     idsOfIncorrectAnswers
+    // );
+  };
+
+  return (
+    <section className="my-14">
+      <QuestionsList questions={questions} preview />
+      <div className="container flex md:max-w-[70%] space-x-2">
+        <button
+          className={`${classes.submitButton} self-start -ml-8`}
+          onClick={handleFormSubmit}
+        >
+          Submit
+        </button>
+        <Link href="/form/new">
+          <a className="self-start">
+            <button className={`${classes.editButton}`}>Edit</button>
+          </a>
+        </Link>
+      </div>
+    </section>
+  );
 };
 
 export default Preview;
